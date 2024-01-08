8ddebb459c9a8366982354b0eb70b600d8b791b1
Huraira <huraira.eqeel@gmail.com>
2024-01-08 18:14:48 +0500

    Update Arrayof_struct.cpp

--- Object-Oriented-Programming/Structs/Arrayof_struct.cpp
+++ Object-Oriented-Programming/Structs/Arrayof_struct.cpp
@@ -21,25 +21,22 @@ int main()
     library l1;
     l1.libid=1;
     l1.libname="Central Library";
-    l1.b[0].bookid=101;
-    l1.b[0].title="C++";
-    l1.b[0].price=500;
-    l1.b[1].bookid=102;
-    l1.b[1].title="Java";
-    l1.b[1].price=600;
-    l1.b[2].bookid=103;
-    l1.b[2].title="Python";
-    l1.b[2].price=700;
-    cout<<"Library ID: "<<l1.libid<<endl;
-    cout<<"Library Name: "<<l1.libname<<endl;
-    cout<<"Book ID: "<<l1.b[0].bookid<<endl;
-    cout<<"Book Title: "<<l1.b[0].title<<endl;
-    cout<<"Book Price: "<<l1.b[0].price<<endl;
-    cout<<"Book ID: "<<l1.b[1].bookid<<endl;
-    cout<<"Book Title: "<<l1.b[1].title<<endl;
-    cout<<"Book Price: "<<l1.b[1].price<<endl;
-    cout<<"Book ID: "<<l1.b[2].bookid<<endl;
-    cout<<"Book Title: "<<l1.b[2].title<<endl;
-    cout<<"Book Price: "<<l1.b[2].price<<endl;
+ //use loop to enter data in array of structure
+    for(int i=0;i<3;i++)
+    {
+        cout<<"Enter Book ID: ";
+        cin>>l1.b[i].bookid;
+        cout<<"Enter Book Title: ";
+        cin>>l1.b[i].title;
+        cout<<"Enter Book Price: ";
+        cin>>l1.b[i].price;
+    }
+//use loop to display data in array of structure
+    for(int i=0;i<3;i++)
+    {
+        cout<<"Book ID: "<<l1.b[i].bookid<<endl;
+        cout<<"Book Title: "<<l1.b[i].title<<endl;
+        cout<<"Book Price: "<<l1.b[i].price<<endl;
+    }
  return 0;
 }

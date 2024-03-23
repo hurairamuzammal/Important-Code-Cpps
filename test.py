9aa27e06031b78e79bbcc13e7dd048890071cfb4
Huraira Muzammal <huraira.eqeel@gmail.com>
2024-03-24 00:17:45 +0500

    test game

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ def print_board(board):
         print(" ".join(row))
 
 def check_win(board):
+    # check horizontal spaces
     for row in board:
         if row.count(row[0]) == len(row) and row[0] != ' ':
             return True
 
+    # check vertical spaces
     for col in range(len(board)):
         check = []
         for row in board:
@@ -14,6 +16,7 @@ def check_win(board):
         if check.count(check[0]) == len(check) and check[0] != ' ':
             return True
 
+    # check diagonals
     if board[0][0] == board[1][1] == board[2][2] != ' ':
         return True
     if board[0][2] == board[1][1] == board[2][0] != ' ':
@@ -28,11 +31,16 @@ def tictactoe():
     while True:
         print_board(board)
         print("Player", player, "turn")
-        row = int(input("Enter row: "))
-        col = int(input("Enter column: "))
+        row = int(input("Enter row (0-2): "))
+        col = int(input("Enter column (0-2): "))
+        
+        if row not in [0, 1, 2] or col not in [0, 1, 2]:
+            print("Invalid move, try again.")
+            continue
         if board[row][col] != ' ':
             print("Invalid move, try again.")
             continue
+
         board[row][col] = player
         if check_win(board):
             print("Player", player, "wins!")

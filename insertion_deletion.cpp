f6621adf88331ab57db79d161fd26c9776eff4aa
Huraira <huraira.eqeel@gmail.com>
2024-06-18 21:08:50 +0500

    insertion_deletion

---
+++ insertion_deletion.cpp
@@ -0,0 +1,87 @@
+// leetcode problem order of 1 insertion and deletion
+
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<unordered_set>
+
+
+class RandomizedCollection {
+private:
+    std::vector<int> nums;
+    std::unordered_map<int, std::unordered_set<int>> m;
+
+public:
+    RandomizedCollection() {
+        // Constructor initializes the data structures.
+    }
+    
+    bool insert(int val) {
+        bool alreadyExists = m.find(val) != m.end() && !m[val].empty();
+        nums.push_back(val);
+        m[val].insert(nums.size() - 1);
+        return !alreadyExists;
+    }
+    bool remove(int val) {
+    if (m.find(val) == m.end() || m[val].empty()) {
+        return false;
+    }
+    
+    int pos = *m[val].begin();
+    int lastElement = nums.back();
+    int lastPos = nums.size() - 1;
+    
+    if (val == lastElement) {
+        // Special case: the element to be removed is the same as the last element
+        nums.pop_back(); // Remove the last element from `nums`
+        m[val].erase(lastPos);
+        if (m[val].empty()) {
+            m.erase(val);
+        }
+    } else {
+        // General case: the element to be removed is not the last element
+        nums[pos] = lastElement; // Move the last element to the position of `val`
+        nums.pop_back(); // Remove the last element from `nums`
+        
+        m[lastElement].insert(pos);
+        m[lastElement].erase(lastPos);
+        
+        m[val].erase(pos);
+        if (m[val].empty()) {
+            m.erase(val);
+        }
+    }
+    
+    return true;
+}
+    int getRandom() {
+        int randomIndex = rand() % nums.size();
+        return nums[randomIndex];
+    }
+};
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+
+
+int main()
+{
+    RandomizedCollection* obj = new RandomizedCollection();
+    bool param_1 = obj->insert(1);
+    bool param_2 = obj->insert(2);
+    bool param_3 = obj->insert(3);
+    bool param_4 = obj->insert(4);
+    bool param_5 = obj->insert(5);
+    bool param_6 = obj->remove(3);
+    bool param_7 = obj->remove(4);
+    bool param_8 = obj->remove(5);
+    bool param_9 = obj->remove(2);
+    bool param_10 = obj->remove(1);
+    int param_11 = obj->getRandom();
+    return 0;
+}
---
+++ integer_to_roman.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+void integerToRoman(int num)
+{
+// here I	1
+// I	1
+// V	5
+// X	10
+// L	50
+// C	100
+// D	500
+// M	1000
+    string m[] = {"", "M", "MM", "MMM"};
+    string c[] = {"", "C", "CC", "CCC", "CD", "D",
+                   "DC", "DCC", "DCCC", "CM"};
+    string x[] = {"", "X", "XX", "XXX", "XL", "L",
+                   "LX", "LXX", "LXXX", "XC"};
+    string i[] = {"", "I", "II", "III", "IV", "V",
+                   "VI", "VII", "VIII", "IX"};
+    string thousands = m[num / 1000];
+    string hundereds = c[(num % 1000) / 100];
+    string tens = x[(num % 100) / 10];
+    string ones = i[num % 10];
+    string ans = thousands + hundereds + tens + ones;
+    cout << ans << endl;
+}
+int main()
+{ 
+    int num = 3549;
+    integerToRoman(num);
+    return 0; 
+}
